e69eb082bd733110b8b0aedda2f8d65482d0542a
quatunee <768860579@qq.com>
2020-02-07 15:01:43 +0800

    测试

---
+++ test.py
@@ -0,0 +1,16 @@
+#!D:\CodeProjects\PythonProjects\opms\.venv\Scripts\python.exe
+#encoding:utf-8
+'''
+@File    :   test.py
+@Time    :   2020/02/07 13:30:54
+@Author  :   DesignerA 
+@Version :   1.0
+@Contact :   DesignerA@qq.com
+@WebSite :   www.cnblogs.com/DesignerA
+'''
+# Start typing your code from here
+
+
+print("Hello World!")
+for i in range(10):
+    print(i)
